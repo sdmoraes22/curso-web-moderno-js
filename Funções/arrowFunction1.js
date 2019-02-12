5ebdbf2935a6d7dbec58e02c5ca242b2dffd4d8f
Cristiano Moraes <cmoraes.ti@gmail.com>
2019-02-12 09:20:51 -0200

    Conceito inicial de arrow function js

---
+++ Funções/arrowFunction1.js
@@ -0,0 +1,19 @@
+let dobro = function (a) {
+    return a * 2
+}
+
+dobro = (a) => {
+    return a * 2
+}
+
+dobro = a => a * 2 // return implícito
+console.log(dobro(Math.PI))
+
+let ola = function () {
+    return 'Olá'
+}
+
+ola = () => 'Olá'
+ola = _ => 'Olá' // possui um param
+
+console.log(ola())
