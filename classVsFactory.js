2a11645fc12adf653771acf3c017b80b44e950a5
sdmoraes22 <cmoraes.ti@gmail.com>
2019-03-25 23:05:28 -0300

    Comparativo entre classe e factory function

---
+++ classVsFactory.js
@@ -0,0 +1,22 @@
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    falar() {
+        console.log(`Meu nome e ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('Cristiano')
+p1.falar()
+
+const criarPessoa = nome => {
+    return {
+        falar: () => console.log(`Meu nome e ${nome}`)
+    }
+}
+
+const p2 = criarPessoa('Joao')
+p2.falar()
+
