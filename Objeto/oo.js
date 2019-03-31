da789762a86f5e7b8a0dafdeac0b36bdbb8849a7
sdmoraes22 <cmoraes.ti@gmail.com>
2019-03-30 21:09:12 -0300

    ntrducao e conceitos de orientacao a objetos

---
+++ Objeto/oo.js
@@ -0,0 +1,22 @@
+//CODIGO NAO EXECUTAVEL
+
+//Procedural
+processamento(valor1, valor2, valor3)
+
+//OO
+objeto =  {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Principios importantes
+// 1- Abstracao
+// 2- Encapsulamento
+// 3- Heranca (prototype)
+// 4- Polimorfismo
