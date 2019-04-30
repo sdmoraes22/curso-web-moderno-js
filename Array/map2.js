fd736055360c63548a7065b1152e345ecd5c888b
sdmoraes22 <cmoraes.ti@gmail.com>
2019-04-29 22:38:56 -0300

    Exercício sobre map

---
+++ Array/map2.js
@@ -0,0 +1,21 @@
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45}',
+    '{ "nome": "Caderno", "preco": 13.90}',
+    '{ "nome": "Kit de Lápis", "preco": 41.22}',
+    '{ "nome": "Caneta", "preco": 7.50}',
+]
+
+// Retornar um array apenas com os preços
+const precos = carrinho.map(function(e) {
+    let carrinhosObj = JSON.parse(e)
+    return carrinhosObj.preco
+})
+
+console.log(precos)
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+
+console.log(resultado)
